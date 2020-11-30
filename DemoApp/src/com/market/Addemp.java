a93b573a57e934420c25d1335195ad115a9b11e5
rizwan <sr0279308@gmail.com>
2020-11-30 11:48:48 +0530

    commit

--- DemoApp/src/com/market/Addemp.java
+++ DemoApp/src/com/market/Addemp.java
@@ -21,8 +21,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-@WebServlet("/Addemp")
 public class Addemp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
